1bbbf6f1753a6381cf5d08cb9ac89da629169ff3
AlexandrLi <muzikant1990@gmail.com>
2016-02-14 22:55:50 +0600

    Add product filter and static method for filtering

--- src/main/java/com/epam/alexandrli/paintballshop/service/ProductService.java
+++ src/main/java/com/epam/alexandrli/paintballshop/service/ProductService.java
@@ -3,11 +3,22 @@ package com.epam.alexandrli.paintballshop.service;
 import com.epam.alexandrli.paintballshop.entity.Product;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ProductService {
     public static final Comparator<Product> ID_ORDER = new IdComparator();
     public static final Comparator<Product> PRICE_ORDER = new PriceComparator();
     public static final Comparator<Product> AVAILABILITY_ORDER = new AvailabilityComparator();
+
+    public static Predicate<Product> isAvailable() {
+        return predicate -> predicate.getAvailableQuantity() > 0;
+    }
+
+    public static List<Product> filterProducts(List<Product> products, Predicate<Product> predicate) {
+        return products.stream().filter(predicate).collect(Collectors.<Product>toList());
+    }
 
     private static class PriceComparator implements Comparator<Product> {
         @Override
