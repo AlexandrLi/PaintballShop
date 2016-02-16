94e1e634233aee976b445e089a966487445d3983
AlexandrLi <muzikant1990@gmail.com>
2016-02-17 01:19:25 +0600

    remove value variable from Characteristic class

--- src/main/java/com/epam/alexandrli/paintballshop/entity/Characteristic.java
+++ src/main/java/com/epam/alexandrli/paintballshop/entity/Characteristic.java
@@ -1,21 +1,12 @@
 package com.epam.alexandrli.paintballshop.entity;
 
 public class Characteristic extends NamedEntity {
-    private String value;
 
     public Characteristic() {
     }
 
     public Characteristic(Integer id, String name, String value) {
         super(id, name);
-        this.value = value;
     }
 
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 }
