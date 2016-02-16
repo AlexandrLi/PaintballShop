6c4acd895b08b9eed03717c4694c4378fac6cbc2
AlexandrLi <muzikant1990@gmail.com>
2016-02-17 01:16:33 +0600

    Add CharacteristicValue class

---
+++ src/main/java/com/epam/alexandrli/paintballshop/entity/CharacteristicValue.java
@@ -0,0 +1,16 @@
+package com.epam.alexandrli.paintballshop.entity;
+
+public class CharacteristicValue {
+    private String value;
+
+    public CharacteristicValue() {
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
