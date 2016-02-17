76c041a2adcab51d5e3885de8828892179d43d4b
AlexandrLi <muzikant1990@gmail.com>
2016-02-18 01:45:56 +0600

    Add new variable productType

--- src/main/java/com/epam/alexandrli/paintballshop/entity/Characteristic.java
+++ src/main/java/com/epam/alexandrli/paintballshop/entity/Characteristic.java
@@ -1,12 +1,21 @@
 package com.epam.alexandrli.paintballshop.entity;
 
 public class Characteristic extends NamedEntity {
+    ProductType type;
 
     public Characteristic() {
     }
 
-    public Characteristic(Integer id, String name, String value) {
+    public Characteristic(Integer id, String name, ProductType type) {
         super(id, name);
+        this.type = type;
     }
 
+    public ProductType getType() {
+        return type;
+    }
+
+    public void setType(ProductType type) {
+        this.type = type;
+    }
 }
