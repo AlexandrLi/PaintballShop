13d012325846ae6f3443bd6eb7c1eabf50000a8e
AlexandrLi <muzikant1990@gmail.com>
2016-02-18 01:11:25 +0600

    generate equals & hashCode

--- src/main/java/com/epam/alexandrli/paintballshop/entity/BaseEntity.java
+++ src/main/java/com/epam/alexandrli/paintballshop/entity/BaseEntity.java
@@ -18,4 +18,20 @@ public abstract class BaseEntity {
     public void setId(int id) {
         this.id = id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
