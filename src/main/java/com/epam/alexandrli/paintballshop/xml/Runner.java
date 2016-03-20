0324403e5360f4912ed247a6aa116729286433e1
Alexandr.li <muzikant1990@gmail.com>
2016-03-20 21:58:46 +0600

    fix Runner.class

--- src/main/java/com/epam/alexandrli/paintballshop/xml/Runner.java
+++ src/main/java/com/epam/alexandrli/paintballshop/xml/Runner.java
@@ -1,25 +1,11 @@
 package com.epam.alexandrli.paintballshop.xml;
 
-import com.epam.alexandrli.paintballshop.entity.UserProfile;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import java.io.IOException;
-import java.util.List;
+import com.epam.alexandrli.paintballshop.entity.Product;
 
 public class Runner {
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
+    public static void main(String[] args) throws SAXEntityParserException {
+        SAXEntityParser saxEntityParser = new SAXEntityParser();
+        Product parsedProduct = saxEntityParser.parse("products.xml", Product.class);
 
-        SAXParser saxParser = factory.newSAXParser();
-        MyHandler handler = new MyHandler();
-        saxParser.parse(Runner.class.getClassLoader().getResourceAsStream("users.xml"), handler);
-
-        List<UserProfile> users = handler.getUsers();
-        for (UserProfile user : users) {
-            System.out.println(user);
-        }
     }
 }
