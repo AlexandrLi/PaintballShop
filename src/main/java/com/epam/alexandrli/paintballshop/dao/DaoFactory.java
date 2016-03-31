a55c7d2e591f53b730ecc1859634a488ed1a1b6e
Alexandr.li <muzikant1990@gmail.com>
2016-03-31 23:50:14 +0600

    Rename AbstractDaoFactory to DaoFactory. Implements AutoCloseable. Declare abstract methods

---
+++ src/main/java/com/epam/alexandrli/paintballshop/dao/DaoFactory.java
@@ -0,0 +1,25 @@
+package com.epam.alexandrli.paintballshop.dao;
+
+import java.sql.SQLException;
+
+public abstract class DaoFactory implements AutoCloseable {
+    public static final int JDBC = 0;
+
+    public static DaoFactory getDAOFactory(int factoryType) throws SQLException {
+        switch (factoryType) {
+            case JDBC:
+                return new JdbcDaoFactory();
+            default:
+                return null;
+        }
+    }
+
+    public abstract GenericDao getDao(Class clazz);
+
+    public abstract void beginTransaction() throws DaoException;
+
+    public abstract void commit() throws DaoException;
+
+    public abstract void rollback() throws DaoException;
+
+}
