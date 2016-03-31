e3cbddbb0f428df42f99519a9dc407b2053e84a6
Alexandr.li <muzikant1990@gmail.com>
2016-04-01 04:29:15 +0600

    Demo class for testing

--- src/main/java/com/epam/alexandrli/paintballshop/dao/Runner.java
+++ src/main/java/com/epam/alexandrli/paintballshop/dao/Runner.java
@@ -1,27 +1,30 @@
 package com.epam.alexandrli.paintballshop.dao;
 
 import com.epam.alexandrli.paintballshop.entity.Gender;
+import com.epam.alexandrli.paintballshop.entity.User;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Runner {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         JdbcDaoFactory jdbcDaoFactory = new JdbcDaoFactory();
-        GenericDao dao = jdbcDaoFactory.getDAO(Gender.class);
-//        Address address = new Address();
-//        address.setCountry("Kazakhstan");
-//        address.setCity("Karaganda");
-//        address.setStreet("Alihanova");
-//        address.setBuildingNumber("34/2");
-//        address.setApartmentNumber("37");
-//        Gender gender = new Gender();
-//        gender.setName("Мужчина");
-//        dao.insert(gender);
-//        gender.setName("Женщина");
-//        dao.insert(gender);
-//        System.out.println(gender.getId());
+        GenericDao<Gender> genderDao = jdbcDaoFactory.getDao(Gender.class);
+        GenericDao<User> userDao = jdbcDaoFactory.getDao(User.class);
+        Map<String, String> params = new HashMap<>();
+        params.put("id", "3");
+        List<Gender> allGenders = genderDao.findAll();
+        List<Gender> allGendersOnPage = genderDao.findAll(2, 2);
+        List<Gender> allGenderByParams = genderDao.findAllByParams(params);
+        Gender gender = new Gender();
+        gender.setName("undefined");
+        genderDao.insert(gender);
+        genderDao.delete(7);
+        Gender genderByPK = genderDao.findByPK(3);
+        gender.setName("none");
+        genderDao.update(gender);
 
-        Gender gender1 = (Gender) dao.read(3);
-        System.out.println(gender1.getName() + " " + gender1.getId());
     }
 }
