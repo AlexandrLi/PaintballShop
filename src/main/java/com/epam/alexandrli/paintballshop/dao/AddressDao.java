d241d0877ced4e88218a33dc833e5091c8bda7c1
Alexandr.li <muzikant1990@gmail.com>
2016-04-02 06:10:02 +0600

    Add AddressDao realization

---
+++ src/main/java/com/epam/alexandrli/paintballshop/dao/AddressDao.java
@@ -0,0 +1,49 @@
+package com.epam.alexandrli.paintballshop.dao;
+
+import com.epam.alexandrli.paintballshop.entity.Address;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AddressDao extends AbstractJdbcDao<Address> {
+
+    public static final String INSERT_ADDRESS = "INSERT address(country, city, street, building_number, apartment_number, user_id) VALUES (?,?,?,?,?,?)";
+    public static final String UPDATE_ADDRESS_BY_ID = "UPDATE address SET country=?, city=?, street=?, building_number=?, apartment_number=? WHERE id=?";
+
+    @Override
+    protected String getQueryForInsert() {
+        return INSERT_ADDRESS;
+    }
+
+    @Override
+    protected String getQueryToUpdateById() {
+        return UPDATE_ADDRESS_BY_ID;
+    }
+
+    @Override
+    protected Address getObjectFromResultSet(ResultSet rs) throws SQLException {
+        Address address = new Address();
+        address.setId(rs.getInt("id"));
+        address.setCountry(rs.getString("country"));
+        address.setCity(rs.getString("city"));
+        address.setStreet(rs.getString("street"));
+        address.setBuildingNumber(rs.getString("building_number"));
+        address.setApartmentNumber(rs.getString("apartment_number"));
+        return address;
+    }
+
+    @Override
+    protected void setVariablesForPreparedStatementExceptId(Address address, PreparedStatement ps) throws SQLException {
+        ps.setString(1, address.getCountry());
+        ps.setString(2, address.getCity());
+        ps.setString(3, address.getStreet());
+        ps.setString(4, address.getBuildingNumber());
+        ps.setString(5, address.getApartmentNumber());
+    }
+
+    @Override
+    protected String getTableName() {
+        return "address";
+    }
+}
