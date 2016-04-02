f1c1013817bfb4ae7c53c8adae74b79de22ba523
Alexandr.li <muzikant1990@gmail.com>
2016-04-03 00:32:09 +0600

    Add realization of datetime conversion to/from db. Fix table name to `order`

--- src/main/java/com/epam/alexandrli/paintballshop/dao/OrderDao.java
+++ src/main/java/com/epam/alexandrli/paintballshop/dao/OrderDao.java
@@ -3,14 +3,12 @@ package com.epam.alexandrli.paintballshop.dao;
 import com.epam.alexandrli.paintballshop.entity.Order;
 import org.joda.time.DateTime;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class OrderDao extends AbstractJdbcDao<Order> {
 
-    public static final String INSERT_ORDER = "INSERT `order`(user_id, date, description) VALUES (?,?,?)";
-    public static final String UPDATE_ORDER_BY_ID = "UPDATE `order` SET date=?, description=? WHERE id=?";
+    public static final String INSERT_ORDER = "INSERT `order`(user_id,created, description) VALUES (?,?,?)";
+    public static final String UPDATE_ORDER_BY_ID = "UPDATE `order` SET created=?, description=? WHERE id=?";
 
     @Override
     protected String getQueryForInsert() {
@@ -26,22 +24,20 @@ public class OrderDao extends AbstractJdbcDao<Order> {
     protected Order getObjectFromResultSet(ResultSet rs) throws SQLException {
         Order order = new Order();
         order.setId(rs.getInt("id"));
-        order.setDate(DateTime.parse(rs.getString("date")));
+        order.setCreated(new DateTime(rs.getTimestamp("created")));
         order.setDescription(rs.getString("description"));
         return order;
     }
 
     @Override
     protected void setVariablesForPreparedStatementExceptId(Order order, PreparedStatement ps) throws SQLException {
-
         ps.setInt(1, order.getUser().getId());
-        // TODO: 02.04.2016 Type conversation for Datetime 
-//        ps.setDate(2, order.getDate().toDateTime());
+        ps.setTimestamp(2, new Timestamp(order.getCreated().getMillis()));
         ps.setString(3, order.getDescription());
     }
 
     @Override
     protected String getTableName() {
-        return "description";
+        return "`order`";
     }
 }
