3769b06c5fb3af64f7864da613bd181a6d6712f5
Alexandr.li <muzikant1990@gmail.com>
2016-04-03 00:29:39 +0600

    Add modified, contentType variables. Set default modified value to DateTime.now(). Generate getters and setters

--- src/main/java/com/epam/alexandrli/paintballshop/entity/Image.java
+++ src/main/java/com/epam/alexandrli/paintballshop/entity/Image.java
@@ -1,7 +1,29 @@
 package com.epam.alexandrli.paintballshop.entity;
 
+import org.joda.time.DateTime;
+
 public class Image extends NamedEntity {
+    // TODO: 02.04.2016 можно и getContentType к сущности прицепить, который будет смотреть на расширение и возвращать соответствующее значение типа image/jpeg из мапы.
+    private DateTime modified;
+    private String contentType;
 
     public Image() {
+        modified = DateTime.now();
+    }
+
+    public DateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(DateTime modified) {
+        this.modified = modified;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
     }
 }
