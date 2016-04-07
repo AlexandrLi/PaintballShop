8fa52b5e097af13595ce92564df6452cf71acc87
Alexandr.li <muzikant1990@gmail.com>
2016-04-08 04:24:18 +0600

    Remove NamedEntity class

--- src/main/java/com/epam/alexandrli/paintballshop/entity/Image.java
+++ src/main/java/com/epam/alexandrli/paintballshop/entity/Image.java
@@ -2,13 +2,22 @@ package com.epam.alexandrli.paintballshop.entity;
 
 import org.joda.time.DateTime;
 
-public class Image extends NamedEntity {
+public class Image extends BaseEntity {
+    private String name;
     // TODO: 02.04.2016 можно и getContentType к сущности прицепить, который будет смотреть на расширение и возвращать соответствующее значение типа image/jpeg из мапы.
     private DateTime modified;
     private String contentType;
 
     public Image() {
         modified = DateTime.now();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public DateTime getModified() {
