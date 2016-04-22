27ec089796b92ccec940474f1d036d7f39da1b96
Alexandr.li <muzikant1990@gmail.com>
2016-04-23 04:51:39 +0600

    Add new methods in services, optimize old ones

--- src/main/java/com/epam/alexandrli/paintballshop/service/ShopService.java
+++ src/main/java/com/epam/alexandrli/paintballshop/service/ShopService.java
@@ -7,6 +7,7 @@ import com.epam.alexandrli.paintballshop.entity.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.epam.alexandrli.paintballshop.dao.DaoFactory.*;
 
@@ -70,5 +71,135 @@ public class ShopService {
             throw new ServiceException("Could not place order", e);
         }
         return cartUser;
+    }
+
+    public List<User> getAllUsersOnPage(int pageNumber, int pageSize) throws ServiceException {
+        List<User> users;
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<User> userDao = jdbcDaoFactory.getDao(User.class);
+            GenericDao<Gender> genderDao = jdbcDaoFactory.getDao(Gender.class);
+            users = userDao.findAll(pageNumber, pageSize);
+            users = users.stream().filter(user -> !user.isDeleted()).collect(Collectors.toList());
+            for (User user : users) {
+                user.setGender(genderDao.findByPK(user.getGender().getId()));
+            }
+        } catch (DaoException e) {
+            throw new ServiceException("Could not get users", e);
+        }
+        return users;
+    }
+
+    public List<Product> getAllProductsOnPage(int pageNumber, int pageSize) throws ServiceException {
+        List<Product> products;
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<Product> productDao = jdbcDaoFactory.getDao(Product.class);
+            GenericDao<ProductType> productTypeDao = jdbcDaoFactory.getDao(ProductType.class);
+            products = productDao.findAll(pageNumber, pageSize);
+            products = products.stream().filter(product -> !product.isDeleted()).collect(Collectors.toList());
+            for (Product product : products) {
+                product.setType(productTypeDao.findByPK(product.getType().getId()));
+            }
+        } catch (DaoException e) {
+            throw new ServiceException("Could not get products", e);
+        }
+        return products;
+    }
+
+    public List<StorageItem> getAllStorageItemsOnPage(int pageNumber, int pageSize) throws ServiceException {
+        List<StorageItem> storageItems;
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<StorageItem> storageItemDao = jdbcDaoFactory.getDao(StorageItem.class);
+            GenericDao<Product> productDao = jdbcDaoFactory.getDao(Product.class);
+            GenericDao<Storage> storageDao = jdbcDaoFactory.getDao(Storage.class);
+            storageItems = storageItemDao.findAll(pageNumber, pageSize);
+            storageItems = storageItems.stream().filter(storageItem -> !storageItem.isDeleted()).collect(Collectors.toList());
+            for (StorageItem storageItem : storageItems) {
+                storageItem.setProduct(productDao.findByPK(storageItem.getProduct().getId()));
+                storageItem.setStorage(storageDao.findByPK(storageItem.getStorage().getId()));
+            }
+        } catch (DaoException e) {
+            throw new ServiceException("Could not get storage items", e);
+        }
+        return storageItems;
+    }
+
+    public List<Order> getAllOrdersOnPage(int pageNumber, int pageSize) throws ServiceException {
+        List<Order> orders;
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<Order> orderDao = jdbcDaoFactory.getDao(Order.class);
+            GenericDao<User> userDao = jdbcDaoFactory.getDao(User.class);
+            GenericDao<OrderStatus> orderStatusDao = jdbcDaoFactory.getDao(OrderStatus.class);
+            GenericDao<OrderItem> orderItemDao = jdbcDaoFactory.getDao(OrderItem.class);
+            GenericDao<Product> productDao = jdbcDaoFactory.getDao(Product.class);
+            orders = orderDao.findAll(pageNumber, pageSize);
+            orders = orders.stream().filter(order -> !order.isDeleted()).collect(Collectors.toList());
+            for (Order order : orders) {
+                order.setUser(userDao.findByPK(order.getUser().getId()));
+                order.setStatus(orderStatusDao.findByPK(order.getStatus().getId()));
+                List<OrderItem> orderItems = orderItemDao.findAllByParams(Collections.singletonMap("order_id", String.valueOf(order.getId())));
+                orderItems = orderItems.stream().filter(orderItem -> !orderItem.isDeleted()).collect(Collectors.toList());
+                for (OrderItem orderItem : orderItems) {
+                    orderItem.setProduct(productDao.findByPK(orderItem.getProduct().getId()));
+                }
+                order.setOrderItems(orderItems);
+            }
+        } catch (DaoException e) {
+            throw new ServiceException("Could not get orders", e);
+        }
+        return orders;
+    }
+
+    public int getUsersCount() throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<User> userDao = jdbcDaoFactory.getDao(User.class);
+            return userDao.getNotDeletedCount();
+        } catch (DaoException e) {
+            throw new ServiceException("Could not get users count", e);
+        }
+    }
+
+    public int getProductsCount() throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<Product> productDao = jdbcDaoFactory.getDao(Product.class);
+            return productDao.getNotDeletedCount();
+        } catch (DaoException e) {
+            throw new ServiceException("Could not get products count", e);
+        }
+    }
+
+    public int getOrdersCount() throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<Order> orderDao = jdbcDaoFactory.getDao(Order.class);
+            return orderDao.getNotDeletedCount();
+        } catch (DaoException e) {
+            throw new ServiceException("Could not get orders count", e);
+        }
+    }
+
+    public int getStorageItemsCount() throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<StorageItem> storageItemDao = jdbcDaoFactory.getDao(StorageItem.class);
+            return storageItemDao.getNotDeletedCount();
+        } catch (DaoException e) {
+            throw new ServiceException("Could not get storage items count", e);
+        }
+    }
+
+    public void deleteUserById(String id) throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<User> userDao = jdbcDaoFactory.getDao(User.class);
+            userDao.delete(Integer.valueOf(id));
+        } catch (DaoException e) {
+            throw new ServiceException("Could not delete user", e);
+        }
+    }
+
+    public void deleteProductById(String id) throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<Product> productDao = jdbcDaoFactory.getDao(Product.class);
+            productDao.delete(Integer.valueOf(id));
+        } catch (DaoException e) {
+            throw new ServiceException("Could not delete user", e);
+        }
     }
 }
--- src/main/java/com/epam/alexandrli/paintballshop/service/UserService.java
+++ src/main/java/com/epam/alexandrli/paintballshop/service/UserService.java
@@ -18,19 +18,45 @@ import static com.epam.alexandrli.paintballshop.dao.DaoFactory.getDaoFactory;
 public class UserService {
 
     public UserService() {
-
     }
 
-    public User registerUser(User user) throws ServiceException {
+    public User registerUser(User user, Address address) throws ServiceException {
         User registeredUser;
         try (DaoFactory jdbcDaoFactory = new JdbcDaoFactory()) {
-            GenericDao<User> userDao = jdbcDaoFactory.getDao(User.class);
-            registeredUser = userDao.insert(user);
-
+            try {
+                jdbcDaoFactory.beginTransaction();
+                GenericDao<Address> addressDao = jdbcDaoFactory.getDao(Address.class);
+                GenericDao<User> userDao = jdbcDaoFactory.getDao(User.class);
+                Address registeredAddress = addressDao.insert(address);
+                user.setAddress(registeredAddress);
+                registeredUser = userDao.insert(user);
+            } catch (DaoException e) {
+                jdbcDaoFactory.rollback();
+                throw new ServiceException("Could not register user", e);
+            }
+            jdbcDaoFactory.commit();
         } catch (DaoException e) {
-            throw new ServiceException("Could not register user", e);
+            throw new ServiceException("Could not init factory", e);
         }
         return registeredUser;
+    }
+
+    public Address getUserAddress(User user) throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = new JdbcDaoFactory()) {
+            GenericDao<Address> addressDao = jdbcDaoFactory.getDao(Address.class);
+            return addressDao.findByPK(user.getAddress().getId());
+        } catch (DaoException e) {
+            throw new ServiceException("Could not read user address from db", e);
+        }
+    }
+
+    public Gender getUserGender(User user) throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = new JdbcDaoFactory()) {
+            GenericDao<Gender> genderDao = jdbcDaoFactory.getDao(Gender.class);
+            return genderDao.findByPK(user.getGender().getId());
+        } catch (DaoException e) {
+            throw new ServiceException("Could not read user gender from db", e);
+        }
     }
 
     public User performUserLogin(String email, String password) throws ServiceException {
@@ -74,7 +100,7 @@ public class UserService {
         return user;
     }
 
-    public User transferCash(User user, String cashAmount) throws ServiceException {
+    public User refillBalance(User user, String cashAmount) throws ServiceException {
         try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
             GenericDao<User> userDao = jdbcDaoFactory.getDao(User.class);
             Money totalCash = user.getCash().plus(Money.parse("KZT " + cashAmount));
@@ -95,18 +121,27 @@ public class UserService {
             GenericDao<Product> productDao = jdbcDaoFactory.getDao(Product.class);
             orders = orderDao.findAllByParams(Collections.singletonMap("user_id", String.valueOf(id)));
             for (Order order : orders) {
-                OrderStatus orderStatus = orderStatusDao.findByPK(orderDao.findColumnByPK("order_status_id", order.getId()));
+                order.setStatus(orderStatusDao.findByPK(order.getStatus().getId()));
                 List<OrderItem> orderItems = orderItemDao.findAllByParams(Collections.singletonMap("order_id", String.valueOf(order.getId())));
                 for (OrderItem orderItem : orderItems) {
-                    Product product = productDao.findByPK(orderItemDao.findColumnByPK("product_id", orderItem.getId()));
-                    orderItem.setProduct(product);
+                    orderItem.setProduct(productDao.findByPK(orderItem.getProduct().getId()));
                 }
-                order.setStatus(orderStatus);
                 order.setOrderItems(orderItems);
             }
         } catch (DaoException e) {
             throw new ServiceException("Could not get user orders", e);
         }
         return orders;
+    }
+
+    public Address updateUserAddress(Address address) throws ServiceException {
+        try (DaoFactory jdbcDaoFactory = getDaoFactory(JDBC)) {
+            GenericDao<Address> addressDao = jdbcDaoFactory.getDao(Address.class);
+            addressDao.update(address);
+        } catch (DaoException e) {
+            throw new ServiceException("Could not update user", e);
+        }
+        return address;
+
     }
 }
