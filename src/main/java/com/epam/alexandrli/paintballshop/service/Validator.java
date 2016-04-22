9e42dfcf0b8f3c5b2f58b1cbd533b83bd449bc42
Alexandr.li <muzikant1990@gmail.com>
2016-04-23 04:50:37 +0600

    Rename regexp, add new

--- src/main/java/com/epam/alexandrli/paintballshop/service/Validator.java
+++ src/main/java/com/epam/alexandrli/paintballshop/service/Validator.java
@@ -1,10 +1,11 @@
 package com.epam.alexandrli.paintballshop.service;
 
 public class Validator {
-    public static final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    public static final String PASSWORD_REGEX = "^.{6,}$";
-    public static final String NAME_REGEX = "^\\p{L}+{1,}$";
-    public static final String PHONE_NUMBER_REGEX = "^\\+(?:[0-9]?){6,14}[0-9]$";
+    public static final String EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    public static final String PASSWORD = "^.{6,}$";
+    public static final String NOT_EMPTY_TEXT = "^\\p{L}+{1,}$";
+    public static final String NOT_EMPTY_NUMBER = "^\\p{N}+{1,}$";
+    public static final String PHONE_NUMBER = "^\\+(?:[0-9]?){6,14}[0-9]$";
 
 
     public boolean validate(final String input, String regex) {
