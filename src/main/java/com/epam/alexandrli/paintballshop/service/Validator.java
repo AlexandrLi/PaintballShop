48cc8f151de42101492bd5ee90ddd81084469179
Alexandr.li <muzikant1990@gmail.com>
2016-04-28 05:05:11 +0600

    Add regex variables

--- src/main/java/com/epam/alexandrli/paintballshop/service/Validator.java
+++ src/main/java/com/epam/alexandrli/paintballshop/service/Validator.java
@@ -5,6 +5,9 @@ public class Validator {
     public static final String PASSWORD = "^.{6,}$";
     public static final String NOT_EMPTY_TEXT = "^\\p{L}+{1,}$";
     public static final String NOT_EMPTY_NUMBER = "^\\p{N}+{1,}$";
+    public static final String PRODUCT_AMOUNT = "^[1-9]\\d{0,3}$";
+    public static final String STORAGE_AMOUNT = "^\\d{1,4}$";
+    public static final String MONEY = "^[1-9]\\d{0,8}$";
     public static final String PHONE_NUMBER = "^\\+(?:[0-9]?){6,14}[0-9]$";
 
 
