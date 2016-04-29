1d3309182ef4d047b3e9ef6d2f016fa609fe27a1
Alexandr.li <muzikant1990@gmail.com>
2016-04-30 04:49:22 +0600

    Add AppContextListener for init and close connection pool

---
+++ src/main/java/com/epam/alexandrli/paintballshop/servlet/AppContextListener.java
@@ -0,0 +1,25 @@
+package com.epam.alexandrli.paintballshop.servlet;
+
+import com.epam.alexandrli.paintballshop.connectionpool.ConnectionPool;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        DataSource pool = ConnectionPool.getInstance();
+        servletContext.setAttribute("pool", pool);
+    }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        ConnectionPool pool = (ConnectionPool) servletContext.getAttribute("pool");
+        pool.close();
+    }
+}
